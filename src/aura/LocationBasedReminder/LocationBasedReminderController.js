3852daaa74fdb437b3ab5b9c5cce624cc1e454b8
Karanraj <karanrajforu@gmail.com>
2016-08-07 22:52:01 -0400

    init

---
+++ src/aura/LocationBasedReminder/LocationBasedReminderController.js
@@ -0,0 +1,28 @@
+({
+    doinit : function(component, event, helper) {
+        console.log('failed');
+        if(navigator.geolocation){
+            console.log('failed');
+            navigator.geolocation.getCurrentPosition(function(position){     
+                component.set("v.comLat",position.coords.latitude);
+                component.set("v.comLong",position.coords.longitude);
+            });	
+        }
+    },
+    handleValueChange : function(component, event, helper) {
+        console.log(component.get("v.comLat"));
+        var action = component.get("c.openRecords");
+        action.setParams({
+            lati : component.get("v.comLat"),
+            longi: component.get("v.comLong")
+        });      
+        action.setCallback(this,function(response){
+            var state = response.getState();
+            if(state === 'SUCCESS'){
+                component.set("v.TaskRecords",response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+        
+    }
+})
--- src/aura/RecentRecordsApp/RecentRecordsApp.app
+++
@@ -1,3 +0,0 @@
-<aura:application >
-    <c:RecentRecords object="Lead" fields="Id,Name,Phone" limit="5" />
-</aura:application>
